70a7237189c14eda5f2977e8f1806434750f4d6d
MatzeKausM <imbapriest@hotmail.de>
2017-11-07 14:02:08 +0100

    Übung 07.11.2017 import ergänzt

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoProperties.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoProperties.java
@@ -1,5 +1,12 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@ConfigurationProperties(prefix="cnj.info")
+
 public class InfoProperties {
 
 }
