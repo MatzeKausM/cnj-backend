ed144515d49c4176010e8febaef7f295be389d6c
MatzeKausM <imbapriest@hotmail.de>
2017-11-28 14:19:59 +0100

    swagger fertig

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -5,13 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
 @RestController
 @RequestMapping("/v1/info")
+@Api
 public class InfoController {
 	@Autowired
 	InfoProperties properties;
 
 	@GetMapping
+	@ApiOperation(value = "Show server information", response = Info.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Server reachable") })
 	public Info getInfo() {
 		Info result = new Info();
 		result.setMessage(properties.getMessage());
