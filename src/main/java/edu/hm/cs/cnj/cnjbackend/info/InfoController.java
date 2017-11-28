4965ecba289f4d8756617f0ab7a6558c0541d0c2
MatzeKausM <imbapriest@hotmail.de>
2017-11-28 14:09:29 +0100

    swagger

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,21 +1,20 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/info")
 public class InfoController {
-	
 	@Autowired
 	InfoProperties properties;
-	
-	@RequestMapping("/info")
+
+	@GetMapping
 	public Info getInfo() {
 		Info result = new Info();
 		result.setMessage(properties.getMessage());
 		return result;
 	}
 }
-
-
